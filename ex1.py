d02d242b7385e820e69811b335ad0c4429aee35d
Thatanmol <anmolsharma13223@gmail.com>
2024-08-16 11:46:39 +1200

    Add files via upload
    week 4 python example

--- ex1.py
+++ ex1.py
@@ -1,8 +1,29 @@
-from datetime import *
-import pytz      
+# Initialize a global counter Registration IDs
+registration_counter = 50000
 
+def student_registration():
+    global registration_counter # use the global counter
 
-tz_INDIA = pytz.timezone('Asia/Kolkata')
-datetime_INDIA = datetime.now(tz_INDIA)
-print("INDIA time:", datetime_INDIA.strftime("%H:%M:%S"))
-      
+    # Generate the Registration ID using the current counter value
+    registration_id = registration_counter
+    registration_counter += 1 # Increment the counter for the next registration
+
+    # collecting information from the user
+    date = input("Enter the registration date(dd/mm/yyyy): ")
+    student_id = input("Enter the student ID:")
+    student_name = input("Enter the student Name: ")
+    course_name = input("Enter the Course Name: ")
+
+# Return the collected information and Registration ID
+return date, student_id, student_name, course_name, registration_id
+
+# call the function and get the data:
+date, student_id, student_name, course_name, registration_id = student_registration()
+
+# Print the information outside the function
+print("\nPrinting Student Registration Information:")
+print(f"Date: {date}")
+print(f"Student ID: {student_id}")
+print(f"student Name: {student_name}")
+print(f"Course Name: {course_name}")
+print(f"Registration ID: {registration_id}")
