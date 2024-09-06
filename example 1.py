36ad1fc0794def572c5ba057ccb022e4e2efc251
Thatanmol <anmolsharma13223@gmail.com>
2024-09-06 12:32:52 +1200

    Add files via upload
    week 7

---
+++ example 1.py
@@ -0,0 +1,23 @@
+class Monster:
+
+     # attributes
+     health = 90
+     energy = 40
+
+     # methods
+     def attack(Self, amount):
+          print('The monster has attacked!')
+          print(f'{amount} damage was dealt')
+          monster.energy -= 20
+          print(monster.energy)
+
+     def move(self, speed):
+          print('The monster has moved')
+          print(f'It has a speed of {speed}')      
+
+monster = Monster()
+monster1 = Monster()
+monster2= Monster()
+monster3 = Monster()
+
+
